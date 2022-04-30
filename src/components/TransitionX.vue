51d0fed5093eaf362549c5bf1f1afd24220f611d
wly <1355498705@qq.com>
2022-04-30 23:57:36 +0800

    平面过度

--- src/components/TransitionX.vue
+++ src/components/TransitionX.vue
@@ -19,19 +19,36 @@
     </div>
     <div class="wrap">
        <transition-group
-      enter-active-class="animate__animated animate__rubberBand"
+      enter-active-class="animate__animated animate__backInUp"
       leave-active-class="animate__animated animate__backOutDown"
     >
       <div class="item" :key="item" v-for="item in list">{{ item }}</div>
     </transition-group>
+    </div>
+
+    <div>
+      <button @click="random">random</button>
+      <div class="wraps">
+         <transition-group
+           move-class="move"
+          >
+            <div class="items" :key="item" v-for="item in lists">{{ item }}</div>
+          </transition-group>
+      </div>
     </div>
    
   </div>
 </template>
 
 <script setup lang="ts">
+import _ from 'lodash'
 const showBox = ref(false);
 const list = reactive<number[]>([1, 2, 3, 4, 5]);
+
+const lists = ref(Array.apply(null,({length:81} as any)).map((item,index) => index))
+
+console.log(lists.value);
+
 
 const add = () => {
   list.push(list.length + 1);
@@ -39,9 +56,27 @@ const add = () => {
 const deletex = () => {
   list.pop();
 };
+
+const random = ()=>{
+  lists.value = _.shuffle(lists.value)
+}
 </script>
 
 <style scoped lang="less">
+
+.wraps{
+  display: flex;
+  flex-wrap: wrap;
+  width: calc(30px * 9 + 18px);
+  .items{
+    width: 30px;
+    height: 30px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 1px solid #ccc;
+  }
+}
 .wrap{
   display: flex;
   flex-wrap: wrap;
@@ -84,5 +119,9 @@ const deletex = () => {
 .fade-leave-to {
   height: 50px;
   width: 50vw;
+}
+
+.move{
+  transition: all 1s;
 }
 </style>
